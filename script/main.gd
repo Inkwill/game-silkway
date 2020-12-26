c4c8827c00458ce437fe243471938ed456e0734e
inkwill <ink.see@gmail.com>
2020-12-26 18:37:00 +0800

    first export

--- script/main.gd
+++ script/main.gd
@@ -2,6 +2,7 @@ extends Node2D
 const SQLite = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
 const account = preload("res://resouce/account.tres")
 
+var db
 var db_name := "res://resouce/db/chinese_calendar"
 
 
@@ -11,20 +12,20 @@ func _ready():
 	MessageBox.message(account.name)
 	$bg/Button.text = account.curplayer.gold as String
 	
-	var db
 	db = SQLite.new()
 	db.path = db_name
 	db.verbose_mode = true
 	db.open_db()
 
-	var select_condition : String = "Population > 10000000"
-	var selected_array : Array = db.select_rows("city", select_condition, ["id", "Name","Population"])
-	print("condition: " + select_condition)
-	print("result: ", selected_array)
-	
 
 func _on_Button_pressed():
 	var player = account.curplayer
 	player.gold += 1
 	#gameManager.current_player.set("gold", gameManager.current_player.gold +1)
 	$bg/Button.text = account.curplayer.gold as String
+
+	var select_condition : String = "emperor_id == %s" % account.curplayer.gold
+	var selected_array : Array = db.select_rows("t_emperor_names", select_condition, ["name"])
+	print("condition: " + select_condition)
+	for city in selected_array:
+		MessageBox.message(city["name"])
