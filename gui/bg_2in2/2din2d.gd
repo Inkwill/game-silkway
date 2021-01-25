2bb1134f9b80b753e0214dab6c0d171086869aaa
inkwill <ink.see@gmail.com>
2021-01-26 02:37:01 +0800

    test ui

--- gui/bg_2in2/2din2d.gd
+++ gui/bg_2in2/2din2d.gd
@@ -6,22 +6,22 @@ var scale_factor := Vector2(1,1)
 var tweentime = 1.0
 onready var content = $Viewport/bg2in2/content
 onready var camera = $Viewport/bg2in2/Camera2D
-var content_height
-var ground_height = 108.0 # under the floor
+
 
 func _ready():
 	var err = get_viewport().connect("size_changed", self, "_on_root_size_changed")
 	if err : push_warning("Connect err : _on_root_size_changed")
-	content_height = view_size.y  - content.position.y
 #	yield(get_tree().create_timer(1.0),"timeout")
-	
+
+func move_content(to):
+	$Tween.interpolate_property(content,"position:y",content.position.y,to, tweentime,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	if not $Tween.is_active():	$Tween.start()
+
 func zoom(scale:Vector2):
 	scale_factor = Vector2(max(0.1,min(scale.x,1)),max(0.1,min(scale.y,1)))
 	$Tween.interpolate_property(camera,"zoom",camera.zoom, scale_factor, tweentime,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($ViewTexture,"rect_size",$ViewTexture.rect_size, view_size*scale_factor, tweentime,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Tween.interpolate_property(content,"position:y",content.position.y,view_size.y*scale_factor.y-content_height+ground_height*(1-scale_factor.y+0.1) , tweentime,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	if not $Tween.is_active():	$Tween.start()
-
 
 func _on_root_size_changed():
 #	The viewport is resized depending on the window height.
--- scene/main.gd
+++ scene/main.gd
@@ -16,31 +16,26 @@ func _ready():
 	
 	if player.assetid == -1:
 		Effect.new("gain",account.asseter.create_member()).at(player)
-	$bt_move.connect("pressed",self,"move")
+
 	date.connect("timer_step",self,"refresh")
+	$bt_move.connect("pressed",self,"open_move")
 	account.curday = 1670231
 	print(account.world.get_aero().sunshine_time())
 	print(GameDate.get_time_name(account.world.get_aero().sunrise()/96))
 	print(GameDate.get_time_name(account.world.get_aero().sunset()/96))
 	refresh()
 
-func move():
-	host.goto_scene("res://scene/map.tscn")
 
 func refresh(_delta=null):
 	$Button.text = account.curday as String
-	$bt_Date.text = date.full_name(account.curday)
-	$lb_gold.text = GameDate.get_time_name(account.curday)
+#	$lb_date.text = date.full_name(account.curday)
+#	$lb_gold.text = GameDate.get_time_name(account.curday)
 #	$Viewport/background/DirectionalLight.light_energy = GameDate.get_time(account.curday)/6.0
 	
 func _on_Button_pressed():
 	var eff = Effect.new("add",{"gold":1})
 	eff.at(account.asseter.members.values())
-#	if $bg2.scale_factor == Vector2(1,1): $bg2.zoom(Vector2(1,0.1))
-#	else : $bg2.zoom(Vector2(1,1))
-	if not date.is_running :
-		var action = Action.new(player,{"timer":12},true)
-		action.act()
+	$bg2.move_content( (randi()%5+2)*100)
 	refresh()
 	
 #	var uiaction = load(_path_ui_action).instance()
@@ -51,6 +46,11 @@ func _on_bt_Date_pressed():
 	var d = GameDate.date_from_juliandate(account.curday)
 	MessageBox.message("%s-%s-%s" % [d["year"],d["month"],d["day"]])
 
+func open_move():
+	var winMap = load("res://scene/map.tscn").instance()
+	add_child(winMap)
+	$bg2.move_content(-60)
+	
 func _exit_tree():
 	pass
 
