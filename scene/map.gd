560eae95a4c9f7d7da054bb63a934a3764061935
inkwill <ink.see@gmail.com>
2021-01-25 18:00:37 +0800

    reimplement the display of player in maptile

--- scene/map.gd
+++ scene/map.gd
@@ -3,8 +3,6 @@ extends Control
 func _ready():
 	$bt_back.connect("pressed",self,"_back_main")
 	$bt_move.connect("pressed",self,"_move")
-	var dater = host.account.date
-	dater.connect("timer_step",self,"_on_timer_step")
 #	$bg2.zoom(Vector2(1.0,0.1))
 	
 func _back_main():
@@ -14,9 +12,5 @@ func _move():
 	var move = null
 	for action in host.account.player.action_list:
 		if action.type == "move" : move = action
-	if move == null : move = Move.new(host.account.player,{"west":35},true)
+	if move == null : move = Move.new(host.account.player,{"north":35},true)
 	move.act()
-
-func _on_timer_step(_delta):
-	$maptile._refresh_map(host.account.world.get_aero())
-	$maptile._refresh_actor()
--- scene/maptile.gd
+++ scene/maptile.gd
@@ -3,9 +3,13 @@ extends Control
 var _aero
 var player
 onready var tilemap = $TileMap
-onready var actormap = $ActorMap
+onready var ui_player = $Player
 
 func _ready():	
+	var dater = host.account.date
+	var err = dater.connect("timer_step",self,"_on_timer_step")
+	if err : push_warning("GameDate connect err[%s] from maptile.gd" % err)
+	
 	_refresh_map(host.account.world.get_aero())
 	_refresh_actor()
 
@@ -18,12 +22,14 @@ func _refresh_map(aero):
 	
 func _refresh_actor():
 	player = host.account.player
-	var old_cell = actormap.get_used_cells_by_id(actormap.tile_set.find_tile_by_name("player"))
-	for old in old_cell:
-		actormap.set_cell(old.x,old.y,-1)
-	var cell_pos_player = _aero.cell_pos(player.pos)
-#	print("cell_pos_player%s->%s" %[player.pos,cell_pos_player])
-	actormap.set_cell(cell_pos_player.x,cell_pos_player.y,actormap.tile_set.find_tile_by_name("player"))
+	ui_player.position = _actor_pos(player.pos)
+	print("actor position: %s"% player.pos)
+#	var old_cell = actormap.get_used_cells_by_id(actormap.tile_set.find_tile_by_name("player"))
+#	for old in old_cell:
+#		actormap.set_cell(old.x,old.y,-1)
+#	var cell_pos_player = _aero.cell_pos(player.pos)
+##	print("cell_pos_player%s->%s" %[player.pos,cell_pos_player])
+#	actormap.set_cell(cell_pos_player.x,cell_pos_player.y,actormap.tile_set.find_tile_by_name("player"))
 
 func _on_gui_input(event):
 	if event.is_pressed():
@@ -32,3 +38,14 @@ func _on_gui_input(event):
 		var cell_id = randi() % 4
 		tilemap.set_cell(pos.x,pos.y,cell_id)
 		_aero.active_cell(pos,cell_id)
+
+func _actor_pos(pos):
+	var world = host.account.world
+	var offset = pos-_aero.pos
+	var center = rect_size *0.5
+	printerr(Vector2(center.x+ rect_size.x * offset.x * world.world_size.x,center.y - rect_size.y * offset.y * world.world_size.y))
+	return Vector2(center.x+ rect_size.x * offset.x * world.world_size.x,center.y - rect_size.y * offset.y * world.world_size.y)
+
+func _on_timer_step(_delta):
+	_refresh_map(host.account.world.get_aero())
+	_refresh_actor()
