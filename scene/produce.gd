d96ea9ead2e1da25a3feed87edbfca1ec236be6e
inkwill <ink.see@gmail.com>
2021-02-02 22:41:07 +0800

    complete produce.tscn

--- scene/produce.gd
+++ scene/produce.gd
@@ -1,25 +1,27 @@
 extends Control
 
 var history_dates := [] # date
-var processed_dates := [] # date
+var processed_list := [] # key
 
 func _ready():
 	history_dates = host.account.incidenter.incidents.keys()
 	history_dates.sort()
-#	host.account.incidenter.connect("processed",self,"_on_incident_processed")
 	refresh_list()
 
 func refresh_list():
-	for element in history_dates:
-		var lab = Label.new()
-		lab.name = element
-		lab.text = "date:%s keys:%s"%[element,host.account.incidenter.incidents[element]]
-		$list_root.add_child(lab)
+	for date in history_dates:
+		var keys = host.account.incidenter.get_incident(date)
+		for key in keys:
+			if not key in processed_list and not $list_root.has_node(key): 
+				var lab = Label.new()
+				lab.name = key
+				lab.text = "date:%s key:%s"%[date,key]
+				$list_root.add_child(lab)
+			elif key in processed_list and $list_root.has_node(key):
+				$list_root.get_node(key).queue_free()
 
 func _on_Button_pressed():
 	process_history()
-#	while inc is GDScriptFunctionState:
-#		var incident = inc.resume()
 
 func process_history(): 
 	for date in history_dates :
@@ -28,9 +30,12 @@ func process_history():
 			for key in keys:
 				var incident = Incident.new(key)
 				incident.connect("progress",self,"_on_incident_processed")
-				var elem = $list_root.get_node(yield(incident,"processed").date)
-				if elem != null : elem.queue_free()
+				processed_list.append(yield(incident,"processed").id)
 				printerr("Processed ok : %s" % key)
+				refresh_list()
 				
-func _on_incident_processed(id,rate):
-	print("process incident[%s] : %s " % [id,rate])
+func _on_incident_processed(id,progress,total):
+	$Label.text = host.account.incidenter.value(id).option1
+	$ProgressBar.max_value = total
+	$ProgressBar.value = progress
+#	print("process incident[%s] : %s " % [id,progress/total])
--- script/data/incident.gd
+++ script/data/incident.gd
@@ -9,12 +9,9 @@ var data
 func _init(id):
 	data = host.account.incidenter.value(id)
 	process()
-#	var p = process_step(10)
-#	while p is GDScriptFunctionState:
-#		print("process incident[%s] : %s " % [_data["id"],yield(self,"progress")])
 
 func process():
 	for i in range(1,11):
 		yield(host.tree.create_timer(0.1),"timeout")
-		emit_signal("progress",data.id,i/10.0)
+		emit_signal("progress",data.id,i,10.0)
 	emit_signal("processed",data)
